f6ad9a5acfa9aafb307c6f07acf727591eee2aa3
first_demo_contribution <mandalnilambar678@gamil.com>
2026-05-17 15:23:59 +0530

    Add cookie-parser and cors middleware; implement asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: './.env'
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error) => {
+        console.log("Express Server Error: ", error);
+        throw error;
+    });
+app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+})
+})
+.catch()
